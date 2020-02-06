a6862728df88559b4f8a4967c58bb6df8008371d
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-02-06 11:58:20 +0000

    ex2

---
+++ basic/ex2.py
@@ -0,0 +1,10 @@
+class Parrot:
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def sing(song):
