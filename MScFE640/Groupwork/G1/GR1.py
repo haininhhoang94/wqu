ba3a92f602b18edb8a9f7e9f218ad799e087950c
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-03-16 22:37:39 +0700

    MScFE650-2

---
+++ MScFE640/Groupwork/G1/GR1.py
@@ -0,0 +1,15 @@
+#%%
+import pandas as pd
+import numpy as np
+
+#%%
+# Import exchange/stock data
+
+# %%
+# Initial variable
+beta_XLE = 1.07
+beta_XLI = 1.06
+R_f = 0.025
+R_m = 0.09
+sigma_market = 0.15
+day_per_year = 252
