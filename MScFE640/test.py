cef51e239afe1e2cf37459d83ccdd8295368660b
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-03-20 00:05:26 +0700

    test new method

--- MScFE640/test.py
+++ MScFE640/test.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use('TKAgg')
 import seaborn as sns
 %matplotlib qt
 
---
+++ MScFE640/test2.py
@@ -0,0 +1,9 @@
+##{
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib qt
+
+##}
+
+
