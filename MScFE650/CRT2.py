181c66539d072410bac79b786b5eec8831046a16
Hai Ninh Hoang <haininhhoang94@gmail.com>
2020-05-10 01:33:41 +0700

    CRT2-Machine Learning

---
+++ MScFE650/CRT2.py
@@ -0,0 +1,21 @@
+#%%
+
+def hey():
+    """Testing
+    :returns: TODO
+
+    """
+    print("Hello World")
+    return 0
+
+#%%
+hey()
+
+def testhaha(hehe):
+    """def
+
+    :hehe: TODO
+    :returns: TODO
+
+    """
+    return 0
---
+++ MScFE650/help-siew.py
@@ -0,0 +1,12 @@
+## Problem: Face Recognition
+import numpy as np
+from matplotlib import pylab as plt
+from sklearn.decomposition import PCA
+from Ipython.display import display
+from Ipython.display import Image
+from Ipython.display import HTML
+from skimage import io
+
+ic = 
+
+
